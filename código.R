152f9cf622be430db9197f4e06cf7a4c3b6bc19a
Ivo Giulietti <ivog@asafondos.org.sv>
2019-07-01 08:48:03 -0600

    Inicio de capacitación

--- código.R
+++ código.R
@@ -2,31 +2,43 @@
 if (statement) {
         #hacer algo
 }
+if (condition) {
+        
+}
 
 x<-data.frame(var1=1:5,var2=6:10,var3= 11:15)
 class(x)
 x
-# este es un loop
-if (x$var1==2) {
+
+# este es un if
+if (any(x$var1==2)==TRUE) {
         x$var4<-x$var1*2
 }
+if (any(x$var1==10)==TRUE) {
+        x$var5<-x$var1*2
+}
+
 
 #otras condiciones
 which(x==2)
+which(x$var1==2)
 x[[2]]
 x[4,16]
-x$var1[which(x$var1==2)]
+x[x$var3==14,]
+which(x$var4==2)
 #######
 #######fechas 
 fecha<-Sys.Date()
 class(fecha)
 quarters(fecha)
-fecha2<-Sys.Date()+1
+fecha2<-Sys.Date()+10
 
 dias<-fecha2-fecha
 dias
 
 texto <- c("January 10, 2012 10:40", "December 9, 2011 9:10")
+texto2<-c("01/12/2019","01/12/2019")
+i<-(texto2 , "%B/%d/%Y")
 class(texto)
 p <- strptime(texto, "%B %d, %Y %H:%M")
 p
@@ -35,8 +47,8 @@ zona<-strptime(p,format="%H:%M",tz="EST")
 zona
 formato
 
-o <- c("1jan1960", "2jan1960", "31mar1960", "30jul1960")
-z <- as.Date(o, "%d%b%Y")
+o <- c("1-jan-1960", "2-jan-1960", "31-mar-1960", "30-jul-1960")
+z <- as.Date(o, "%d-%b-%Y")
 z<-format(as.Date(z),"%B-%Y")
 z
 Sys.Date()
@@ -50,4 +62,7 @@ Sys.Date()
 
 
         
-set.seed(12)
+set.seed(5)
+rnorm(10)
+
+x[sample(nrow(x),2),]
